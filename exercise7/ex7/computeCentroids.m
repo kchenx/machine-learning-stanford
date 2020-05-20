4ca8a0340ccc284c4de561a1969460afca601d41
Kevin Chen <kevin.chen.krc@gmail.com>
2020-05-20 17:34:32 -0400

    k-clustering

--- exercise7/ex7/computeCentroids.m
+++ exercise7/ex7/computeCentroids.m
@@ -10,31 +10,11 @@ function centroids = computeCentroids(X, idx, K)
     %   assigned to it.
     %
 
-    % Useful variables
-    [m n] = size(X);
-
-    % You need to return the following variables correctly.
+    n = size(X, 2);
     centroids = zeros(K, n);
-
-
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Go over every centroid and compute mean of all points that
-    %               belong to it. Concretely, the row vector centroids(i, :)
-    %               should contain the mean of the data points assigned to
-    %               centroid i.
-    %
-    % Note: You can use a for-loop over the centroids to compute this.
-    %
-
-
-
-
-
-
-
-
-    % =============================================================
-
+    for k = 1:K
+        inds = find(idx == k);
+        centroids(k, :) = mean(X(inds, :));
+    end
 
 end
-
